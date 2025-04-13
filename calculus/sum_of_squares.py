ad4f346263e5e8934689bd699682146af839e084
MovingJu <baramishe@gmail.com>
2025-04-14 05:26:31 +0900

    add image

--- calculus/sum_of_squares.py
+++ calculus/sum_of_squares.py
@@ -9,14 +9,15 @@ n = 50
 
 squares = []
 result = []
-for n in range(2, 5000, 5):
+for n in range(100, 1000, 5):
     for i in range(1, n + 1):
 
         squares.append(math.pi/n * Func(0 + i*math.pi/n))
     
     result.append(sum(squares))
+    squares.clear()
 
-x = np.arange(2, 5000, 5)
+x = np.arange(100, 1000, 5)
 y = result
 
 fig, ax = plt.subplots()
@@ -25,6 +26,6 @@ Sin = ax.plot(x, y)
 
 plt.title(f"sum of squares by n")
 
-plt.savefig(f"calculus/plots/sums", format = 'png', dpi = 200)
+plt.savefig(f"calculus/plots/sums.png", format = 'png', dpi = 200)
 
 plt.show()
